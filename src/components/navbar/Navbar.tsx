98a1ee236ba2ce1057639b135bb8283b23d442fe
Lautaro Camejo <lautarocamejo@gmail.com>
2025-11-03 15:16:00 -0300

    upd BrowserRouter implemetation

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,23 @@
 import { navbarLinks } from "@/constants/navbarLinks";
-
+import { BrowserRouter, Link } from "react-router-dom";
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-white shadow-md px-6 py-4 sticky top-0 z-50">
-      <h1 className="text-xl font-bold text-gray-800">☕ CashRegisterApp</h1>
-      <div className="flex justify-around">
-        {navbarLinks.map((link) => (
-          <a
-            key={link.name}
-            href={`localhost:5173/${link.redirect}`}
-            className="px-8 text-gray-700 hover:text-red-500 transition-colors "
-          >
-            {link.name}
-          </a>
-        ))}
-      </div>
-    </nav>
+    <BrowserRouter>
+      <nav className="flex justify-between items-center bg-white shadow-md px-6 py-4 sticky top-0 z-50">
+        <h1 className="text-xl font-bold text-gray-800">☕ CashRegisterApp</h1>
+        <div className="flex justify-around">
+          {navbarLinks.map((link) => (
+            <Link
+              className="px-8 "
+              key={link.name}
+              to={`localhost:5173${link.redirect}`}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </BrowserRouter>
   );
 }
 
